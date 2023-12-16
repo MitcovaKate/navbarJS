19479888f38b147b6920c29ebd0be684eb692c82
Mitcova Kate <131910543+MitcovaKate@users.noreply.github.com>
2023-12-16 17:46:30 +0200

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,9 +1,12 @@
-let menu = new Menu([
-    new MenuItem("Brand","/"),
-    new MenuItem("+123456789","tel:+123456789"),
-    new MenuItem("Services","/our-services"),
-    new MenuItem("Team","/our-team"), 
-    new MenuItem("Contacts","/our-contacts"),
+let menu = new MenuMain([
+    new MenuItem("/","Brand","bi bi-boombox"),
+    new MenuItem("tel:+123456789","+123456789","bi bi-telephone"),
+    new MenuItem("/our-services","Services"),
+    new MenuItem("/our-team","Team"), 
+    new MenuItem("/our-contacts","Contacts","bi bi-person-lines-fill"),
 ])
 let headerTop = document.querySelector('.top')
 headerTop.innerHTML = menu.render()
+/* <i class="bi bi-boombox"></i>
+<i class="bi bi-telephone"></i>
+<i class="bi bi-person-lines-fill"></i> */
